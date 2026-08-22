7bec37d560f3e4fb92238eb1f16364af98214e12
taprewards0-spec <taprewards0@gmail.com>
2026-08-23 00:26:16 +0100

    Create page.js

---
+++ page.js
@@ -0,0 +1,11 @@
+    export default function Home() {
+      return (
+        <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-black text-white">
+          <h1 className="text-4xl font-bold">TapBumber</h1>
+          <p className="mt-4">Earn rewards by tapping!</p>
+          <button className="mt-8 px-8 py-4 bg-green-500 text-white rounded-lg text-2xl">
+            TAP
+          </button>
+        </main>
+      )
+    }
