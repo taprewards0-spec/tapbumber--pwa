833ab787e969de0f8d4c3f6e6ee3a7ff050ee2a4
taprewards0-spec <taprewards0@gmail.com>
2026-08-23 02:47:15 +0100

    Update layout.js

--- layout.js
+++ layout.js
@@ -2,7 +2,7 @@ import './globals.css'
 
 export const metadata = {
   title: 'TapBumber',
-  description: 'Tap and earn 24/7',
+  description: 'Tap to earn BUMBER',
 }
 
 export default function RootLayout({ children }) {
